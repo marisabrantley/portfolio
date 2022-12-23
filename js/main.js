4fe0a244520582df47265d5f1beedeeecf04fc63
MARISA BRANTLEY <marisakbrantley@gmail.com>
2022-12-22 21:06:22 -0800

    Update mobile HTML and CSS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var year = new Date().getFullYear();
+const year = new Date().getFullYear();
 
-var date = `&copy;${year} Marisa Brantley &verbar; All rights reserved.<br>Made with <span>♥</span> in California.`;
+const date = `&copy;${year} Marisa Brantley &verbar; All rights reserved.<br>Made with <span>♥</span> in California.`;
 
 document.getElementsByTagName('footer')[0].innerHTML = date;
