eaea27243fb3fce0b9ba1db2a8bffde75a1148ec
MARISA BRANTLEY <marisakbrantley@gmail.com>
2023-01-10 20:12:01 -0800

    Update html and css for tablet and desktop versions

--- js/main.js
+++
@@ -1,5 +0,0 @@
-const year = new Date().getFullYear();
-
-const date = `&copy;${year} Marisa Brantley &verbar; All rights reserved.<br>Made with <span>♥</span> in California.`;
-
-document.getElementsByTagName('footer')[0].innerHTML = date;
