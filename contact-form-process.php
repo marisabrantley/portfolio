7178868ebe2cb3772b9ed92acc627278fc134de5
MARISA BRANTLEY <marisakbrantley@gmail.com>
2023-01-28 17:36:16 -0800

    Update PHP and JS for contact form

--- contact-form-process.php
+++ contact-form-process.php
@@ -1,80 +1,55 @@
 <?php
-if (isset($_POST['Email'])) {
 
-    $email_to = "marisakbrantley@gmail.com";
-    $email_subject = "Let's Connect";
+$errors = [];
+$errorMessage = '';
 
-    function problem($error)
-    {
-        echo "There were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br><br>";
-        echo $error . "<br><br>";
-        echo "Please go back and fix these errors so we can connect.<br><br>";
-        die();
+$secret = '6LfbKjIkAAAAAH0VkmF5HC58PktPwg1Fp2-at-oq';
+
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+
+    $recaptchaUrl = "https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$recaptchaResponse}";
+    $verify = json_decode(file_get_contents($recaptchaUrl));
+
+    if (!$verify->success) {
+      $errors[] = 'Recaptcha failed';
     }
 
-    // validation expected data exists
-    if (
-        !isset($_POST['First Name']) ||
-        !isset($_POST['Last Name']) ||
-        !isset($_POST['Email']) ||
-        !isset($_POST['Message'])
-    ) {
-        problem('Oops. There seems to be a problem with the form you submitted.');
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
     }
 
-    $first_name = $_POST['First Name']; // required
-    $email = $_POST['Email']; // required
-    $message = $_POST['Message']; // required
-
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-    if (!preg_match($email_exp, $email)) {
-        $error_message .= "The Email address you entered doesn't seem to be valid.<br>";
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
     }
 
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if (!preg_match($string_exp, $first_name)) {
-        $error_message .= 'The First Name you entered does not appear to be valid.<br>';
-    }
-    if (!preg_match($string_exp, $last_name)) {
-        $error_message .= 'The Last Name you entered does not appear to be valid.<br>';
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
     }
 
-    if (strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+
+    if (!empty($errors)) {
+        $allErrors = join('<br/>', $errors);
+        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    } else {
+        $toEmail = 'example@example.com';
+        $emailSubject = 'New email from your contact form';
+        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=utf-8'];
+
+        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+        $body = join(PHP_EOL, $bodyParagraphs);
+
+        if (mail($toEmail, $emailSubject, $body, $headers)) {
+            header('Location: thank-you.html');
+        } else {
+            $errorMessage = "<p style='color: red;'>Oops, something went wrong. Please try again later</p>";
+        }
     }
-
-    if (strlen($error_message) > 0) {
-        problem($error_message);
-    }
-
-    $email_message = "Form details below.\n\n";
-
-    function clean_string($string)
-    {
-        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-        return str_replace($bad, "", $string);
-    }
-
-    $email_message .= "First Name: " . clean_string($first_name) . "\n";
-    $email_message .= "Last Name: " . clean_string($last_name) . "\n";
-    $email_message .= "Email: " . clean_string($email) . "\n";
-    $email_message .= "Message: " . clean_string($message) . "\n";
-
-    // create email headers
-    $headers = 'From: ' . $email . "\r\n" .
-        'Reply-To: ' . $email . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);
-?>
-
-    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
-
-    Thank you so much for your message! I'll be in touch with you soon.
-
-<?php
 }
+
 ?>
