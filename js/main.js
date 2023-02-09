0ac2728ed8b682df303e4cdac42e2a58b017550b
MARISA BRANTLEY <marisakbrantley@gmail.com>
2023-02-09 15:37:32 -0800

    Update HTML and CSS with accessibility

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 // Dark Mode
 
+
 const button = document.getElementById('checkbox');
 
+// Create MediaQueryList object
 const useDark = window.matchMedia('(prefers-color-scheme: dark)');
 let darkModeState = useDark.matches;
 
 function toggleDarkMode(state) {
   document.documentElement.classList.toggle('dark-mode', state);
 }
-
+// Dark Mode Applied If User Has Dark Mode Enabled
 toggleDarkMode(useDark.matches);
 
-useDark.addEventListener('change', (e) => toggleDarkMode(e.matches));
+//Listen for Changes in the OS Settings and Users Toggle Choice
+useDark.addEventListener('change', (evt) => toggleDarkMode(evt.matches));
 
 button.addEventListener('change', () => {
-  document.body.classList.toggle('dark-mode');
+  document.documentElement.classList.toggle('dark-mode');
 });
